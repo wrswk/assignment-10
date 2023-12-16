0b5aea25ee1b59df9cbd2c4bc8db91155b04c8d8
wrswk <147695187+wrswk@users.noreply.github.com>
2023-12-16 14:24:23 +0200

    Add files via upload

--- ExceptionHandling-master/ExceptionHandling/Program.cs
+++ ExceptionHandling-master/ExceptionHandling/Program.cs
@@ -6,7 +6,7 @@
         {
             BasicExceptionHandling();
             MultipleExceptionTypes();
-            FinalyBlockUsage();
+            FinallyBlockUsage();
             CustomExceptionClass();
             ExceptionPropagation();
             UsingThrowAndCatch();
@@ -15,36 +15,24 @@
         static void BasicExceptionHandling()
         {
             Console.Write("Enter a number: ");
-            // TODO:
-            // Convert the input to an integer using int.Parse().
-            // Use a try-catch block to handle FormatException if the user enters a non-numeric value.
-            // Display an error message in case of an exception.
-            // Output the input if correct
             try
             {
                 int number = int.Parse(Console.ReadLine());
-                Console.WriteLine("Enter Number:" + number);
-            } catch (FormatException)
+                Console.WriteLine($"You entered: {number}");
+            }
+            catch (FormatException)
             {
                 Console.WriteLine("Error: Please enter a valid number.");
-            } catch (Exception ex)
-            {
-                Console.WriteLine("An error occurred: " + ex.Message);
             }
-
         }
 
         static void MultipleExceptionTypes()
         {
             Console.Write("Enter a number: ");
-            // TODO:
-            // Implement BasicExceptionHandling code with following modification
-            // Include a catch block for OverflowException to handle cases where the number is too large or small for an int.
-            // Display appropriate error messages for different exceptions.
             try
             {
                 int number = int.Parse(Console.ReadLine());
-                Console.WriteLine("enter numbre: " + number);
+                Console.WriteLine($"You entered: {number}");
             }
             catch (FormatException)
             {
@@ -52,34 +40,25 @@
             }
             catch (OverflowException)
             {
-                Console.WriteLine("Error: The entered number is too large or too small.");
+                Console.WriteLine("Error: The entered number is too large or too small for an int.");
             }
-            catch (Exception ex)
-            { Console.WriteLine("An error occured: " + ex.Message); }
         }
 
-        static void FinalyBlockUsage()
+        static void FinallyBlockUsage()
         {
             Console.Write("Enter a number: ");
-            // TODO:
-            // Implement BasicExceptionHandling code with following modification
-            // Add a finally block to the program.
-            // Use the finally block to display a message whether an exception was caught or not.
             try
             {
                 int number = int.Parse(Console.ReadLine());
-                Console.WriteLine("Enter Number:" + number);
+                Console.WriteLine($"You entered: {number}");
             }
             catch (FormatException)
             {
                 Console.WriteLine("Error: Please enter a valid number.");
             }
-            catch (Exception ex)
+            finally
             {
-                Console.WriteLine("An error occurred: " + ex.Message);
-            } finally
-            {
-                Console.WriteLine("Finally block executed. ");
+                Console.WriteLine("Finally block executed, whether exception occurred or not.");
             }
         }
 
@@ -92,47 +71,33 @@
         static void CustomExceptionClass()
         {
             Console.Write("Enter a number: ");
-            // TODO:
-            // Implement BasicExceptionHandling code with following modification
-            // Modify your number input program to throw NegativeNumberException if the user enters a negative number.
-            // Handle this exception in a separate catch block and display an appropriate message.
             try
             {
                 int number = int.Parse(Console.ReadLine());
-
-                if (number <0)
+                if (number < 0)
                 {
                     throw new NegativeNumberException("Error: Negative numbers are not allowed.");
                 }
-                Console.WriteLine("Enter Number:" + number);
-            } catch ( NegativeNumberException ex)
-            {
-                Console.WriteLine(ex.Message);
+                Console.WriteLine($"You entered: {number}");
             }
             catch (FormatException)
             {
                 Console.WriteLine("Error: Please enter a valid number.");
             }
-            catch (Exception ex)
+            catch (NegativeNumberException ex)
             {
-                Console.WriteLine("An error occurred: " + ex.Message);
+                Console.WriteLine(ex.Message);
             }
-           
-
         }
 
         static void ExceptionPropagation()
         {
             Console.Write("Enter a number: ");
-            // TODO:
-            // Implement BasicExceptionHandling code with following modification
-            // Write a function CheckNumber that takes an integer and throws ArgumentOutOfRangeException if the number is greater than 100.
-            // In this function, call CheckNumber inside a try block and handle the exception.
             try
             {
                 int number = int.Parse(Console.ReadLine());
                 CheckNumber(number);
-                Console.WriteLine("Enter Number:" + number);
+                Console.WriteLine($"You entered: {number}");
             }
             catch (FormatException)
             {
@@ -142,36 +107,43 @@
             {
                 Console.WriteLine(ex.Message);
             }
-
         }
+
         static void CheckNumber(int number)
         {
-            try
+            if (number > 100)
             {
-                if (number > 100)
-                {
-                    string logMessage = $"Error: Number {number} is greater than 100.";
-                    Console.WriteLine("Logging: " + logMessage);
-                    throw new ArgumentOutOfRangeException(logMessage);
-                }
+                throw new ArgumentOutOfRangeException("Error: Number cannot be greater than 100.");
             }
-
-
-
-
-
-                    // NOTE: You can implement the CheckNumber here
-
-                    static void UsingThrowAndCatch()
-        {
-            Console.Write("Enter a number: ");
-            // TODO:
-            // Implement BasicExceptionHandling code with following modification
-            // Write a function CheckNumberWithLogging that takes an integer and throws ArgumentOutOfRangeException if the number is greater than 100.
-            // Modify the CheckNumberWithLogging function to log the exception message before throwing it.
-            // In this function, catch the exception in the main program and display the logged message.
         }
 
-        // NOTE: You can implement the CheckNumberWithLogging here
+        static void UsingThrowAndCatch()
+        {
+            Console.Write("Enter a number: ");
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+                CheckNumberWithLogging(number);
+                Console.WriteLine($"You entered: {number}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error: Please enter a valid number.");
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine($"Logged message: {ex.Message}");
+            }
+        }
+
+        static void CheckNumberWithLogging(int number)
+        {
+            if (number > 100)
+            {
+                string logMessage = $"Error: Number {number} is greater than 100.";
+                Console.WriteLine($"Logging: {logMessage}");
+                throw new ArgumentOutOfRangeException(logMessage);
+            }
+        }
     }
 }
